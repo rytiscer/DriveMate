67985659e8c50a7cac085f0c09025a6cfd4888e9
rytiscer <rytis.cerniauskas@gmail.com>
2024-05-13 23:03:03 +0300

    Updated main page content and added styles

--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -1,58 +1,17 @@
-import { Link } from "react-router-dom";
 import image from "../../assets/bmw_logo.png";
+import styles from "../Main/Main.module.scss";
 
 const Main = () => {
   return (
-    <div style={{ display: "flex", alignItems: "center" }}>
-      <div style={{ flex: "1" }}>
-        <h1>Welcome to DriveMate</h1>
-        <p>
-          DriveMate is an internal system designed for a car rental company to
-          streamline the car rental process. This web application allows
-          managing cars, clients, orders, and service events in one place.
-        </p>
-
-        <h2>Key Features</h2>
-        <ul>
-          <li>
-            <strong>Car Management:</strong> Ability to view, add, edit, and
-            delete cars.
-          </li>
-          <li>
-            <strong>Client Management:</strong> Manage client data, add new
-            clients, edit or delete existing ones.
-          </li>
-          <li>
-            <strong>Order Management:</strong> Create new orders, view, edit, or
-            delete existing orders. Each order assigns a car to a client, and
-            the rental price is calculated based on the period the client plans
-            to rent the car.
-          </li>
-          <li>
-            <strong>Car Service:</strong> Register service events, such as
-            technical inspections or oil changes. This helps track the technical
-            condition of cars and plan future maintenance work.
-          </li>
-        </ul>
-
-        <h2>Explore DriveMate</h2>
-        <ul>
-          <li>
-            <Link to="/cars">View Cars</Link>
-          </li>
-          <li>
-            <Link to="/clients">Manage Clients</Link>
-          </li>
-          <li>
-            <Link to="/orders">Manage Orders</Link>
-          </li>
-          <li>
-            <Link to="/services">Car Services</Link>
-          </li>
-        </ul>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1>
+          Welcome to <span className={styles.title}>DriveMate</span>
+        </h1>
+        <h3 className={styles.subtitle}>Streamline car rental management</h3>{" "}
       </div>
-      <div style={{ flex: "1", textAlign: "right" }}>
-        <img src={image} alt="DriveMate Image" style={{ maxWidth: "100%" }} />
+      <div className={styles.image}>
+        <img src={image} alt="DriveMate Image" className={styles.image} />
       </div>
     </div>
   );
